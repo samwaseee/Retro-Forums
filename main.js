77035d7cab454f17c3121c85fc6c2f2ebe5d5ea2
samwaseee <samwaseee@gmail.com>
2024-03-04 12:48:11 +0600

    final

--- main.js
+++ main.js
@@ -6,11 +6,10 @@ const loadPost = async (searchText) => {
         //console.log(typeof posts)
         displayPost(posts);
     }
-    else{
+    else {
         const res = await fetch(`https://openapi.programming-hero.com/api/retro-forum/posts?category=${searchText}`);
         const data = await res.json();
         const posts = data.posts;
-        //console.log(typeof posts)
         displayPost(posts);
     }
 }
@@ -20,17 +19,17 @@ const displayPost = posts => {
     const postDiv = document.getElementById('posts');
 
     postDiv.textContent = ''
-    
+
     posts.forEach(e => {
         const postcard = document.createElement('div');
         postcard.classList = `bg-[#F3F3F5] p-6 rounded-2xl flex mb-5`;
         postcard.innerHTML = `
-        <div class="avatar ${e.isActive ? 'online' : 'offline'} h-24">
-            <div class="w-20 h-20 rounded-xl mt-4 mr-4">
-                <img class=""
-                    src="${e.image}" />
+        <div class="indicator">
+             <span class="indicator-item badge ${e.isActive ? 'badge-success' : 'badge-error'}"></span> 
+             <div class="w-16 md:w-24 rounded-full">
+             <img src="${e.image}" class="rounded-xl"/>
+           </div>
             </div>
-        </div>
         <div class="mt-4 ml-4 lg:w-[596px]">
             <div class="flex font-medium">
                 #<h5 class="mr-7">${e.category}</h5>
@@ -40,11 +39,11 @@ const displayPost = posts => {
             <p class="border-b-2 border-[#12132D40] border-dashed py-4 text-[#12132d75]">${e.description}</p>
             <div class="flex text-[#12132d75] mt-4">
                 <div class="flex grow">
-                    <img src="./images/Group 13.png" class="mr-3">
+                    <img src="./images/Group 13.png" class="md:mr-3">
                 <p>${e.comment_count}</p>
-                <img src="./images/Group 16eye.png" class="mr-3 ml-9">
+                <img src="./images/Group 16eye.png" class="md:mr-3 md:ml-9">
                 <p>${e.view_count}</p>
-                <img src="./images/Group 18time.png" class="mr-3 ml-9">
+                <img src="./images/Group 18time.png" class="mr-3 md:ml-9">
                 <p>${e.posted_time} min</p>
                 </div>
                 <button class="btn-ghost rounded-2xl bt" onclick = "ok('${e.title.replace(/'/g, "\\'")}',${e.view_count})"><img src="./images/Group 40106read-sign.png"></button>
@@ -53,12 +52,14 @@ const displayPost = posts => {
         `
         postDiv.appendChild(postcard);
     });
-
+    ToggleSpinner(false);
 }
 
+let i = document.getElementById('mark').innerText;
 
 function ok(event, view) {
-    console.log(event, view);
+    i++;
+    document.getElementById('mark').innerText = i;
 
     const section = document.createElement('div');
     section.classList = 'flex bg-white rounded-xl p-4 mb-4';
@@ -73,8 +74,12 @@ function ok(event, view) {
 
 
 function search() {
+    document.getElementById('posts').textContent = '';
+    ToggleSpinner(true);
     const searchText = (document.getElementById('searchinput').value);
-    loadPost(searchText);
+    setTimeout(() => {
+        loadPost(searchText);
+    }, 2000);
 }
 
 loadPost(null);
@@ -117,4 +122,9 @@ const displayLatestPost = posts => {
         `
         latestPostDiv.appendChild(postcard)
     });
+}
+
+
+const ToggleSpinner = (isLoading) => {
+    isLoading ? document.getElementById('loading').classList.remove('hidden') : document.getElementById('loading').classList.add('hidden', true);
 }
